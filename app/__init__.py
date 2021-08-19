c6bde270714726bdfc283b4060dbdeeb7b7dfaa9
hdinjos <hdinjos@gmail.com>
2021-08-19 19:49:06 +0700

    make migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 server = Flask(__name__)
 server.debug = True
+server.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///jaya.db"
+db = SQLAlchemy(server)
+migrate = Migrate(server, db)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username= db.Column(db.String(80), unique=True, nullable=False)
+  email= db.Column(db.String(200), unique=True, nullable=False)
+
+  def __repr__(self):
+    return "<User %r>" % self.username
 
 from app import views
