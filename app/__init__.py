a3460c8e43411ad88a5caa6dd9b334b73cad41ac
hdinjos <hdinjos@gmail.com>
2021-08-19 21:26:23 +0700

    change modal to folder

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,4 @@ server.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///jaya.db"
 db = SQLAlchemy(server)
 migrate = Migrate(server, db)
 
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username= db.Column(db.String(80), unique=True, nullable=False)
-  email= db.Column(db.String(200), unique=True, nullable=False)
-
-  def __repr__(self):
-    return "<User %r>" % self.username
-
 from app import views
