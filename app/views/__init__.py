1acc8fe817a4378adc79a096468d7ac667a0b130
hdinjos <hdinjos@gmail.com>
2021-09-20 01:23:24 +0700

    with macros

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -4,8 +4,8 @@ from flask import render_template
 
 @server.route("/")
 def index():
-  return render_template("index.html")
+  return render_template("index.html", title="Home")
 
 @server.route("/table")
 def table():
-  return render_template("table.html")
+  return render_template("table.html", title="Table")
