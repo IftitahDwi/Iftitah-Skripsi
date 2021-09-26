fcf6af1c442b32e56d563f3a236b1106616f25be
hdinjos <hdinjos@gmail.com>
2021-09-27 01:19:44 +0700

    update data done

--- app/views/Load_Data/__init__.py
+++ app/views/Load_Data/__init__.py
@@ -25,3 +25,22 @@ def destroy_load_data(id):
   db.session.delete(data_load_id)
   db.session.commit()
   return redirect(url_for('load_data'))
+
+@server.route('/load-data/edit', methods=['POST', 'GET'])
+def update_load_data():
+  if request.method == 'POST':
+    nidn = request.form['nidn']
+    niy = request.form['niy']
+    load_name = request.form['load_name']
+    gs_id = request.form['gs_id']
+    id = request.form['data_id']
+    data_load = DataLoad.query.filter_by(id=int(id)).first()
+    data_load.nidn = nidn
+    data_load.niy = niy
+    data_load.load_name = load_name
+    data_load.gs_id = gs_id
+    db.session.commit()
+    # request_data = DataLoad(nidn=nidn, niy=niy, load_name=load_name, gs_id=gs_id)
+    # db.session.add(request_data)
+    # db.session.commit()
+  return redirect(url_for('load_data'))
