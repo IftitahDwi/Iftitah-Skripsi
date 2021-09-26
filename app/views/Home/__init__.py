d2d2666607d53434330d76e12881d19073a3443f
hdinjos <hdinjos@gmail.com>
2021-09-27 01:32:53 +0700

    edit home ui

--- app/views/Home/__init__.py
+++ app/views/Home/__init__.py
@@ -1,6 +1,8 @@
 from app import server, db
+from app.models.DataLoad import DataLoad
 from flask import render_template
 
 @server.route("/")
 def index():
-  return render_template("index.html", title="Home")
+  load_data_count = DataLoad.query.count()
+  return render_template("index.html", title="Home", load_data_count=load_data_count)
