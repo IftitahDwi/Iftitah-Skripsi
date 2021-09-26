630ad420007da31a8ddc7aaedfd2a242eb7681ef
hdinjos <hdinjos@gmail.com>
2021-09-26 20:41:40 +0700

    create dataload model

--- app/models/User/__init__.py
+++ app/models/User/__init__.py
@@ -1,10 +1,13 @@
 from app import db
 
-class User(db.Model):
+class DataLoad(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  username= db.Column(db.String(80), unique=False, nullable=False)
-  email= db.Column(db.String(200), unique=False, nullable=False)
+  nidn= db.Column(db.String(20), unique=False, nullable=False)
+  niy= db.Column(db.String(20), unique=False, nullable=False)
+  load_name= db.Column(db.String(50), unique=False, nullable=False)
+  gs_id= db.Column(db.String(20), unique=False, nullable=False)
+  scraped= db.Column(db.Boolean(), unique=False, default=False, nullable=False)
 
   def __repr__(self):
-     return f"User({self.id},{self.username}, {self.email})"
+     return f"User({self.id},{self.load_name}, {self.scraped})"
     # return "<User %r>" % self.username
