45556e7ba1bcc31b48bffea1ee24fd4fead423d4
hdinjos <hdinjos@gmail.com>
2021-09-26 21:15:44 +0700

    delete modules

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,21 @@ server.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///system.db"
 db = SQLAlchemy(server)
 migrate = Migrate(server, db)
 
+from app import db
+
+# class DataLoad(db.Model):
+#   id = db.Column(db.Integer, primary_key=True)
+#   nidn= db.Column(db.String(20), unique=False, nullable=False)
+#   niy= db.Column(db.String(20), unique=False, nullable=False)
+#   load_name= db.Column(db.String(50), unique=False, nullable=False)
+#   gs_id= db.Column(db.String(20), unique=False, nullable=False)
+#   scraped= db.Column(db.Boolean(), unique=False, default=False, nullable=False)
+
+#   def __repr__(self):
+#      return f"User({self.id},{self.load_name}, {self.scraped})"
+#     # return "<User %r>" % self.username
+
+
 from app.views import Home
 from app.views import Load_Data
 # from app import views
