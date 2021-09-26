a6cd5b0ff816b7db3e3bce9d50defd44ae6509e9
hdinjos <hdinjos@gmail.com>
2021-09-26 23:23:07 +0700

    get all data load

--- app/views/Home/__init__.py
+++ app/views/Home/__init__.py
@@ -1,8 +1,6 @@
 from app import server, db
-from app.models.DataLoad import DataLoad
 from flask import render_template
 
 @server.route("/")
 def index():
-  print(DataLoad.query.all())
   return render_template("index.html", title="Home")
--- app/views/Load_Data/__init__.py
+++ app/views/Load_Data/__init__.py
@@ -1,6 +1,8 @@
 from app import server, db
+from app.models.DataLoad import DataLoad
 from flask import render_template
 
 @server.route("/load-data")
 def load_data():
-  return render_template("load_data.html", title="Table")
+  data_load = DataLoad.query.all()
+  return render_template("load_data.html", title="Table", data_load=data_load)
