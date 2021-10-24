9b5e00c594919a3247bf185ffcdfe01ac68f83e0
hdinjos <hdinjos@gmail.com>
2021-10-24 20:11:32 +0700

    done preprocessing

---
+++ app/utils/preprocessing.py
@@ -0,0 +1,39 @@
+import re
+import string
+import fasttext
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory,StopWordRemover, ArrayDictionary
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from nltk.tokenize import word_tokenize
+
+fasttext.FastText.eprint = lambda x: None
+
+def case_folding(data_set=[]):
+    result = list(map(lambda x: dict(x, **{"title": re.sub(r'[^\w\s]', '', re.sub(r"\d+", "", x["title"].lower()).strip())}), data_set))
+    return result
+
+def lang_detect(data_set=[]):
+    DATA_TRAIN_LANGUAGE = "/home/hdinjos/Documents/learn/python/satuan/tutor_flask/app/utils/model_language/lid.176.bin"
+    model = fasttext.load_model(DATA_TRAIN_LANGUAGE)
+    result = list(filter(lambda x: model.predict(x["title"])[0][0][9:] == "id", data_set))
+    return result
+
+def stopword(title_stop):
+    stop_factory = StopWordRemoverFactory().get_stop_words()
+    more_stopword = ['customer', 'relationship','fadegoretas','particle']
+    data = stop_factory + more_stopword
+    dictionary = ArrayDictionary(data)
+    str = StopWordRemover(dictionary)
+    stop = str.remove(title_stop)
+    tokens = word_tokenize(stop)
+    new_tokens = " ".join(tokens)
+    return new_tokens
+
+def stopword_title(data_set=[]):
+    result = list(map(lambda x: dict(x, **{"title": stopword(x["title"])}), data_set))
+    return result
+
+def stemmed(data_set=[]):
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    result = list(map(lambda x: dict(x, **{"title": stemmer.stem(x["title"])}), data_set))
+    return result
--- app/views/Preprocessing/__init__.py
+++ app/views/Preprocessing/__init__.py
@@ -3,13 +3,13 @@ from app.models.DataLoad import DataLoad
 from app.models.Scrapped import Scrapped
 from app.utils.to_dict import object_as_dict
 from flask import render_template, request, redirect, url_for
-from app.utils.case_folding import case_folding
+from app.utils.preprocessing import case_folding, lang_detect, stopword_title, stemmed
+import pandas as pd
 
 @server.route("/preprocessing")
 def preprocessing():
   join_data = db.session.query(Scrapped, DataLoad).filter(DataLoad.id == Scrapped.data_load_id).order_by(Scrapped.id.desc())
   scrapped = []
-  titles = []
   for s, l in join_data:
     scrapped_data = {
       "id": s.id,
@@ -18,10 +18,40 @@ def preprocessing():
       "year": s.year,
       "gs_id": l.gs_id
     }
-    titles.append(s.title)
     scrapped.append(scrapped_data)
+  
+  # print(scrapped)
+  print("####this result case folding start")
+  case_folding_data = case_folding(scrapped)
+  print(len(case_folding_data))
+  print("####this result case folding end")
+
+  print("####this result lang detect start")
+  lang_detect_data = lang_detect(case_folding_data)
+  print(len(lang_detect_data))
+  print("####this result lang detect end")
+
+  print("####this result stopword title start")
+  stopword_data = stopword_title(lang_detect_data)
+  print(len(stopword_data))
+  print("####this result stopword title end")
+
+  print("####this result stemming title start")
+  stemmed_data = stemmed(stopword_data)
+  print(stemmed_data)
+  print("####this result stemming title end")
+  # df_data = pd.DataFrame(stemmed_data)
+  # print(df_data)
+  new_data_set=[]
+  for data in stemmed_data:
+      for new_data in data["title"].split(" "):
+          data_result = {
+          "name": data["name"],
+          "gs_id": data["gs_id"],
+          "title": new_data, "year": data["year"]}
+          new_data_set.append(data_result)
     # print(scrapped.)
-  print(case_folding(titles))
+  # print(lang_detect(case_folding(titles)))
 
   db.session.close()
-  return render_template("preprocessing.html")
+  return render_template("preprocessing.html",result_datas = new_data_set)
