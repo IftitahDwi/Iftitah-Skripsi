ce7fc767638c04c053fd59f8f1180da9c85046e5
hdinjos <hdinjos@gmail.com>
2021-10-27 02:08:58 +0700

    update preprocessing

--- app/views/Preprocessing/__init__.py
+++ app/views/Preprocessing/__init__.py
@@ -20,28 +20,11 @@ def preprocessing():
     }
     scrapped.append(scrapped_data)
   
-  # print(scrapped)
-  print("####this result case folding start")
   case_folding_data = case_folding(scrapped)
-  print(len(case_folding_data))
-  print("####this result case folding end")
-
-  print("####this result lang detect start")
   lang_detect_data = lang_detect(case_folding_data)
-  print(len(lang_detect_data))
-  print("####this result lang detect end")
-
-  print("####this result stopword title start")
   stopword_data = stopword_title(lang_detect_data)
-  print(len(stopword_data))
-  print("####this result stopword title end")
-
-  print("####this result stemming title start")
   stemmed_data = stemmed(stopword_data)
-  print(stemmed_data)
-  print("####this result stemming title end")
-  # df_data = pd.DataFrame(stemmed_data)
-  # print(df_data)
+
   new_data_set=[]
   for data in stemmed_data:
       for new_data in data["title"].split(" "):
@@ -50,8 +33,6 @@ def preprocessing():
           "gs_id": data["gs_id"],
           "title": new_data, "year": data["year"]}
           new_data_set.append(data_result)
-    # print(scrapped.)
-  # print(lang_detect(case_folding(titles)))
 
   db.session.close()
   return render_template("preprocessing.html",result_datas = new_data_set)
